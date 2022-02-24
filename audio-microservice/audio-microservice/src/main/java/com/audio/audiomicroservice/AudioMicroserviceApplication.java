022c43f2e2dfb1435ff1e0159bc3b696023b433b
Joe <joetyler8388@gmail.com>
2022-02-24 09:17:15 -0600

    initial commit of spring init

---
+++ audio-microservice/audio-microservice/src/main/java/com/audio/audiomicroservice/AudioMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.audio.audiomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioMicroserviceApplication.class, args);
+	}
+
+}
---
+++ audio-microservice/audio-microservice/src/test/java/com/audio/audiomicroservice/AudioMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audiomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
